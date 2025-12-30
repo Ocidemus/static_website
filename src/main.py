2e82dae7fa61ecf70262bbd25bf0129e7afde1e6
Ocidemus <codeofascendence@gmail.com>
2025-12-30 15:09:36 +0000

    I: adaptable to github pages

--- src/main.py
+++ src/main.py
@@ -4,20 +4,21 @@ from inline_to_markdown import *
 from markdown_to_blocks import *
 import os
 import shutil
+import sys
 
-def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursively(dir_path_content, template_path, dest_dir_path , basepath):
     for item in os.listdir(dir_path_content):
         s = os.path.join(dir_path_content,item)
         d = os.path.join(dest_dir_path,item)
         if os.path.isdir(s):
             os.makedirs(d, exist_ok=True)
-            generate_pages_recursively(s, template_path, d)
+            generate_pages_recursively(s, template_path, d, basepath)
         else:
             if item.endswith(".md"):
                 dest_file = os.path.splitext(d)[0] + ".html"
-                generate_page(s, template_path, dest_file)  
-    
-def generate_page(from_path, template_path, dest_path):
+                generate_page(s, template_path, dest_file, basepath)  
+
+def generate_page(from_path, template_path, dest_path , basepath):
     print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
     with open(from_path, "r", encoding="utf-8") as f:
         md_content = f.read()
@@ -28,6 +29,10 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md_content)
     template_content = template_content.replace("{{ title }}", title)
     final_content = template_content.replace("{{ content }}", html_content)
+    
+    final_content = final_content.replace("href=\"/", f"href=\"{basepath}/")
+    final_content = final_content.replace("src=\"/", f"src=\"{basepath}/")
+    
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(final_content)
 
@@ -44,7 +49,12 @@ def copy_files(items, source_dir, dest_dir):
             
 def main():
     
-    d = "public"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = ""
+    
+    d = "docs"
     s = "static"
     if os.path.exists(d) and os.path.isdir(d):
             if os.listdir(d):
@@ -53,7 +63,7 @@ def main():
         os.makedirs(d)
     items = os.listdir(s)
     copy_files(items, s, d)
-    generate_pages_recursively("content", "template.html", "public")
+    generate_pages_recursively("content", "template.html", "docs", basepath)
     
     
 
